fd2799a98240bbf451436077086d012651f3bff0
Frank Sanchez <franksanchez132@gmail.com>
2026-09-15 15:46:57 -0500

    Completed job functionality

--- yash.c
+++ yash.c
@@ -21,6 +21,8 @@ typedef struct {
 
 Job jobs[MAX_JOBS]; // max number of jobs;
 int job_count = 0;
+int prev_job_i = -1;
+int current_job_i = -1;
 
 void add_job(pid_t pgid, char* command, char * state){
     if (job_count >= MAX_JOBS){
@@ -36,32 +38,64 @@ void add_job(pid_t pgid, char* command, char * state){
 }
 
 int find_job(pid_t pgid){
+    int job_index = -1;
     for (int i = 0; i < job_count; i++){
         if (jobs[i].pgid == pgid){
-            return i;
+            job_index = i;
+            break;
         }
+        
     }
-    printf("Job not found\n");
+    return job_index;
 }
 
 void handle_status(pid_t pid, int status, char* input){
+    int job_index = find_job(pid);
+
     if (WIFEXITED(status)) {
-        //normal exit sir
-    } 
-    if (WIFSIGNALED(status)) {// ctrl c
-        int sig = WTERMSIG(status);
-        if (sig == SIGINT){
-            write(STDOUT_FILENO, "\n", 1);
+        //normal exit 
+        if (job_index != -1){
+        jobs[job_index].state = "Done";
         }
     } 
+    if (WIFSIGNALED(status)) {// ctrl c
+        if (job_index != -1){
+        int sig = WTERMSIG(status);
+        if (sig == SIGINT){
+            jobs[job_index].state = "Terminated";
+            write(STDOUT_FILENO, "\n", 1);
+        }
+
+        }
+
+    } 
     if (WIFSTOPPED(status)) {
+        
         int sig = WSTOPSIG(status);
         if (sig == SIGTSTP){
-            write(STDOUT_FILENO, "\n", 1);
-            add_job(pid, input, "Stopped");
+            if (job_index == -1){
+            add_job(pid,input, "Stopped");
+                job_index = find_job(pid);
+                prev_job_i = current_job_i;
+                current_job_i = job_index;
+                write(STDOUT_FILENO, "\n", 1);
+
+            }
+            else{
+                jobs[job_index].state = "Stopped";
+                prev_job_i = current_job_i;
+                current_job_i = job_index;
+                write(STDOUT_FILENO, "\n", 1);
+            }
+            
+            
         } 
     }
     if (WIFCONTINUED(status)){
+        int job_index = find_job(pid);
+        if (job_index != -1){
+            jobs[job_index].state = "Running";
+        }
         write(STDOUT_FILENO, "#\n", 1);
 
 
@@ -79,6 +113,7 @@ int main(){
     char *input;
     char *split_string[200];
     char *filename;
+    
 
     int pipefd[2];
 
@@ -96,6 +131,50 @@ int main(){
         int input_fd = -1; //i file directory number 
         int output_fd = -1; //o file directory number
         input = readline("# ");
+        char *original_input = strdup(input); // make a copy of the original input for job management
+
+        for (int i = job_count - 1; i >= 0; i--){
+            if (strcmp(jobs[i].state, "Running") == 0 || strcmp(jobs[i].state, "Stopped") == 0){
+                int status;
+                pid_t child_result = waitpid(jobs[i].pgid,&status, WNOHANG | WUNTRACED );
+                if (child_result == -1){
+                    perror("waitpid failed");
+                }
+                else if (child_result == 0){ 
+                    continue;
+                }
+                else{
+                    handle_status(jobs[i].pgid, status, jobs[i].command);  // small helper, called for each
+                    if (strcmp(jobs[i].state, "Done") == 0 || strcmp(jobs[i].state, "Terminated") == 0){
+                        printf("[%d] - %s %s\n", jobs[i].job_id, jobs[i].state, jobs[i].command);
+                        free(jobs[i].command);
+                        for (int j = i; j < job_count - 1; j++){
+                            jobs[j] = jobs[j + 1];
+                        }
+                        job_count--;
+                        i--; // adjust index after removing job
+
+                        if (current_job_i == i){
+                            current_job_i = -1; // reset current job index if it was the removed job
+                        }
+                        else if (current_job_i > i){
+                            current_job_i--; // adjust current job index if it was after the removed job
+                        }
+
+                        if (prev_job_i == i){
+                            prev_job_i = -1; // reset previous job index if it was the removed job
+                        }
+                        else if (prev_job_i > i){
+                            prev_job_i--; // adjust previous job index if it was after the removed job
+                        }
+                    }
+                    else{
+                    printf("[%d] - %s %s\n", jobs[i].job_id, jobs[i].state, jobs[i].command);
+                }
+            }
+            }
+
+        }
         
         //Parse string
         char* token = strtok(input, " ");
@@ -104,6 +183,58 @@ int main(){
         while (token != NULL){
             token = strtok(NULL, " ");
             split_string[++i] = token;
+        }
+
+    
+
+        if (split_string[0] == NULL){
+            continue;
+        }
+
+        if(strcmp(split_string[0], "jobs") == 0 || strcmp(split_string[0], "jobs\n") == 0){
+            for (int i = 0; i < job_count; i++) {
+                char marker = ' ';
+                if (i == current_job_i) marker = '+';
+                else if (i == prev_job_i) marker = '-';
+                printf("[%d] %c %s   %s\n", jobs[i].job_id, marker, jobs[i].state, jobs[i].command);
+            }
+            continue;
+        }
+        if(strcmp(split_string[0], "fg") == 0 || strcmp(split_string[0], "fg\n") == 0){
+            if (current_job_i != -1){
+                pid_t pgid = jobs[current_job_i].pgid;
+                tcsetpgrp(STDIN_FILENO, pgid); // set terminal to foreground of current job
+                kill(-pgid, SIGCONT);
+                int status;
+                pid_t child_result = waitpid(pgid,&status, WUNTRACED);
+                if (child_result == -1){
+                    perror("waitpid failed");
+                }
+                else{
+                    handle_status(pgid, status, jobs[current_job_i].command);  // small helper, called for each
+                    printf("[%d] - %s %s\n", jobs[current_job_i].job_id, jobs[current_job_i].state, jobs[current_job_i].command);
+
+                }
+                tcsetpgrp(STDIN_FILENO,yash_pgid);
+                continue;
+            }
+            else{
+                printf("No current job to bring to foreground\n");
+                continue;
+            }
+        }
+        if(strcmp(split_string[0], "bg") == 0 || strcmp(split_string[0], "bg\n") == 0){
+            if (current_job_i != -1){
+                pid_t pgid = jobs[current_job_i].pgid;
+                kill(-pgid, SIGCONT); // send signal SIGCONT to process group of current job
+                jobs[current_job_i].state = "Running";
+                printf("[%d] - %s %s\n", jobs[current_job_i].job_id, jobs[current_job_i].state, jobs[current_job_i].command);
+                continue;
+            }
+            else{   
+                printf("No current job to bring to background\n");
+                continue;
+            }
         }
 
         int pipe_i = -1;
@@ -269,7 +400,10 @@ int main(){
         }
         }
         else{ // no pipe but can check for &
-            split_string[background_i] = NULL; // remove & from command
+            if(background_i != -1){
+            split_string[background_i] = NULL;// remove & from command
+            }
+            
             cpid_3 = fork();
         
     
@@ -313,43 +447,43 @@ int main(){
                 
                 execvp(split_string[0], split_string);//(1st string, array of args with null as terminal)
                 perror("execvp"); // only reached if execvp failed
-                exit(1);
+                exit(EXIT_FAILURE);
             }
             else
             { //parent call
-
-            if (background_bool == true){
+                setpgid(cpid_3, cpid_3);
+                if (background_bool == true){
+                    
+                     //group cpid3
+                    add_job(cpid_3, original_input, "Running");
+                    printf("[%d] %d\n", jobs[job_count-1].job_id, cpid_3);
+                    background_bool = false; // reset for next command
+                    
+                    
                 
-            
-                setpgid(cpid_3, cpid_3); //group leader cpid1
-
-                add_job(cpid_3, input, "Running");
-                printf("[%d] %d\n", job_count, cpid_3);
-                
-            
-            }
-            else{
-                setpgid(cpid_3, cpid_3); //group leader cpid1
-
-                tcsetpgrp(STDIN_FILENO, cpid_3);
-                int status;
-                pid_t child3_result =  waitpid(cpid_3,&status, WUNTRACED);
-                
-                if (child3_result == -1){
-                    perror("waitpid failed");
                 }
                 else{
-                    handle_status(cpid_3, status, input);  // small helper, called for each
+                    tcsetpgrp(STDIN_FILENO, cpid_3);
+                    int status;
+                    pid_t child3_result =  waitpid(cpid_3,&status, WUNTRACED);
+                    
+                    if (child3_result == -1){
+                        perror("waitpid failed");
+                    }
+                    else{
+                        handle_status(cpid_3, status, original_input);  // small helper, called for each
+                    }
+
+                    tcsetpgrp(STDIN_FILENO,yash_pgid);
                 }
-
-                tcsetpgrp(STDIN_FILENO,yash_pgid);
-            }
         }
 
         }
+    free(original_input);
     }
     
     free(input);
+    
     
     
     return 0;
